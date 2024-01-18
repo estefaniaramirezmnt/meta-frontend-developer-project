6e055db8de497b37573f06212ff17d133fe1dc05
Estefanía <estefaniaramirezmnt@gmail.com>
2024-01-18 13:59:16 +0100

    Project started: added router, eliminated logo, added components folder and navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import logo from "../images/logo.svg";
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+
+      {/* mobile navbar*/}
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* navbar items*/}
+
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>{" "}
+        <li>
+          <a href="/">Services</a>
+        </li>{" "}
+        <li>
+          <a href="/">Menu</a>
+        </li>{" "}
+        <li>
+          <a href="/">Reservation</a>
+        </li>{" "}
+        <li>
+          <a href="/">Order online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
